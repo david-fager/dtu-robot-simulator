f3d20380de9a8a95e17c730298c8b0bf8f12b916
David Fager <s185120@student.dtu.dk>
2022-03-29 11:34:59 +0200

    multiple robots fix

--- robot-sim/PushEndpoints.cs
+++ robot-sim/PushEndpoints.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System.Net;
 
 namespace robot_sim
 {
@@ -28,10 +27,20 @@ namespace robot_sim
                 batteryResistance = robot.batteryResistance,
                 lastRepairReason = robot.lastRepairReason,
             }).ToList();
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8000/full");
-            if (robots.Count == 1) request.Content = new StringContent(JsonConvert.SerializeObject(data.FirstOrDefault()), System.Text.Encoding.UTF8, "application/json");
-            else request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-            client.Send(request);
+
+            try
+            {
+                foreach (var robot in robots)
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8001/full");
+                    request.Content = new StringContent(JsonConvert.SerializeObject(robot), System.Text.Encoding.UTF8, "application/json");
+                    client.Send(request);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         private static void PushMovement(HttpClient client, int time, List<Robot> robots)
@@ -43,10 +52,19 @@ namespace robot_sim
                 currentPosition = robot.currentPosition,
                 expectedPosition = robot.expectedPosition,
             }).ToList();
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8000/movement");
-            if (robots.Count == 1) request.Content = new StringContent(JsonConvert.SerializeObject(data.FirstOrDefault()), System.Text.Encoding.UTF8, "application/json");
-            else request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-            client.Send(request);
+            try
+            {
+                foreach (var robot in robots)
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8001/movement");
+                    request.Content = new StringContent(JsonConvert.SerializeObject(robot), System.Text.Encoding.UTF8, "application/json");
+                    client.Send(request);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         private static void PushSensor(HttpClient client, int time, List<Robot> robots)
@@ -59,10 +77,20 @@ namespace robot_sim
                 batteryResistance = robot.batteryResistance,
                 lastRepairReason = robot.lastRepairReason,
             }).ToList();
-            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8000/sensor");
-            if (robots.Count == 1) request.Content = new StringContent(JsonConvert.SerializeObject(data.FirstOrDefault()), System.Text.Encoding.UTF8, "application/json");
-            else request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-            client.Send(request);
+
+            try
+            {
+                foreach (var robot in robots)
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8001/sensor");
+                    request.Content = new StringContent(JsonConvert.SerializeObject(robot), System.Text.Encoding.UTF8, "application/json");
+                    client.Send(request);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
