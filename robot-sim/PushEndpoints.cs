6251c880e25443e1725b0a65f705105aa49bd3f7
David Fager <s185120@student.dtu.dk>
2022-04-05 11:24:55 +0200

    Better posting

--- robot-sim/PushEndpoints.cs
+++ robot-sim/PushEndpoints.cs
@@ -9,15 +9,67 @@ namespace robot_sim
 
         public static void PushToEndpoints(int tickSpeed, int time, List<Robot> robots)
         {
-            //if (CheckConnection(tickSpeed))
-            //{
-                using (var client = new HttpClient())
+            using (var client = new HttpClient())
+            {
+                foreach (var robot in robots)
                 {
-                    PushFull(client, time, robots);
-                    PushMovement(client, time, robots);
-                    PushSensor(client, time, robots);
+                    //PushFull(client, time, robot);
+                    //PushMovement(client, time, robot);
+                    PushSensor(client, time, robot);
                 }
-            //}
+            }
+        }
+
+        private static void PushFull(HttpClient client, int time, Robot robot)
+        {
+
+            CallPost(client, "/full", JsonConvert.SerializeObject(new
+            {
+                time = time,
+                robotID = robot.robotID,
+                currentPosition = robot.currentPosition,
+                expectedPosition = robot.expectedPosition,
+                motorTemperature = (robot.motorTemperature * 9 / 5 + 32), // convert to fahrenheit
+                batteryResistance = robot.batteryResistance,
+                //lastRepairReason = robot.lastRepairReason,
+            }));
+        }
+
+        private static void PushMovement(HttpClient client, int time, Robot robot)
+        {
+            CallPost(client, "/movement", JsonConvert.SerializeObject(new
+            {
+                time = time,
+                robotID = robot.robotID,
+                currentPosition = robot.currentPosition,
+                expectedPosition = robot.expectedPosition,
+            }));
+        }
+
+        private static void PushSensor(HttpClient client, int time, Robot robot)
+        {
+            CallPost(client, "/sensor", JsonConvert.SerializeObject(new
+            {
+                time = time,
+                robotID = robot.robotID,
+                motorTemperature = (robot.motorTemperature * 9 / 5 + 32), // convert to fahrenheit
+                batteryResistance = robot.batteryResistance,
+                //lastRepairReason = robot.lastRepairReason,
+            }));
+        }
+
+        private static void CallPost(HttpClient client, string endpoint, string jsonData)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, BaseURL + endpoint);
+                request.Content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                //client.Timeout = TimeSpan.FromMilliseconds(900);
+                client.Send(request);
+            }
+            catch
+            {
+            }
         }
 
         private static bool CheckConnection(int tickSpeed)
@@ -35,64 +87,6 @@ namespace robot_sim
             catch
             {
                 return false;
-            }
-        }
-
-        private static void PushFull(HttpClient client, int time, List<Robot> robots)
-        {
-            foreach (var robot in robots)
-            {
-                var data = new
-                {
-                    time = time,
-                    robotID = robot.robotID,
-                    currentPosition = robot.currentPosition,
-                    expectedPosition = robot.expectedPosition,
-                    motorTemperature = (robot.motorTemperature * 9 / 5 + 32), // convert to fahrenheit
-                    batteryResistance = robot.batteryResistance,
-                    //lastRepairReason = robot.lastRepairReason,
-                };
-
-                var request = new HttpRequestMessage(HttpMethod.Post, BaseURL + "/full");
-                request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-                client.Send(request);
-            }
-        }
-
-        private static void PushMovement(HttpClient client, int time, List<Robot> robots)
-        {
-            foreach (var robot in robots)
-            {
-                var data = new
-                {
-                    time = time,
-                    robotID = robot.robotID,
-                    currentPosition = robot.currentPosition,
-                    expectedPosition = robot.expectedPosition,
-                };
-
-                var request = new HttpRequestMessage(HttpMethod.Post, BaseURL + "/movement");
-                request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-                client.Send(request);
-            }
-        }
-
-        private static void PushSensor(HttpClient client, int time, List<Robot> robots)
-        {
-            foreach (var robot in robots)
-            {
-                var data = new
-                {
-                    time = time,
-                    robotID = robot.robotID,
-                    motorTemperature = (robot.motorTemperature * 9 / 5 + 32), // convert to fahrenheit
-                    batteryResistance = robot.batteryResistance,
-                    //lastRepairReason = robot.lastRepairReason,
-                };
-
-                var request = new HttpRequestMessage(HttpMethod.Post, BaseURL + "/sensor");
-                request.Content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
-                client.Send(request);
             }
         }
     }
